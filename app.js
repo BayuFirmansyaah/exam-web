c53888f5c9f6d5b54d096cf5e1ce3422735c2caf
zulfarizqi <bayufirmansyah2800>
2021-07-01 19:28:35 +0700

    add module routes and server run

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const bodyParser = require("body-parser")
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
+const routes = require('./routes');
 const port = 3000;
 
 
-// using ejs
+// express setting
 app.set("view engine","ejs");
 app.use(expressLayouts)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+
+// use routes
+routes(app);
+
+
+// run server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
